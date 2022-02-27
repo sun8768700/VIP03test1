2623aec65d3943f77892686061db6b1457d0868a
rm <sun8768700@qq.com>
2022-02-27 23:49:44 +0800

    上传测试内容

---
+++ AutoLogger test.java
@@ -0,0 +1,8 @@
+package com.testing.autocommon;
+
+import org.apache.log4j.Logger;
+
+public class AutoLogger {
+	//创建一个静态的logger对象，在全局范围内进行log4J功能的调用。
+	public static final Logger log=Logger.getLogger(AutoLogger.class);
+}
